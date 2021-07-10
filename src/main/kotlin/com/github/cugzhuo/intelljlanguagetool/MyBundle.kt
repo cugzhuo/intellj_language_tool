48effe7be8ddeefd2a5321d1fab902accae20a9a
GitHub Action <action@github.com>
2021-07-10 06:42:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/cugzhuo/intelljlanguagetool/MyBundle.kt
+++ src/main/kotlin/com/github/cugzhuo/intelljlanguagetool/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cugzhuo.intelljlanguagetool
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/cugzhuo/intelljlanguagetool/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cugzhuo.intelljlanguagetool.services
+
+import com.github.cugzhuo.intelljlanguagetool.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
